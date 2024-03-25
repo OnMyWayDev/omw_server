0baa281147f6208a72ee09c84e0416335f8d020d
SeyunJang <ts-seyun.jang@rakuten.com>
2024-03-26 02:11:00 +0900

    add mongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KakaoModule } from './modules/kakao/kakao.module';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [KakaoModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI), //connect to MongoDB, TODO: utilize DB for logging and user auth, current db name : ~/omw
+    //useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
+    KakaoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
