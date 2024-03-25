009121e0778a1be0970df1e8af5b2078b30e8e2c
SeyunJang <ts-seyun.jang@rakuten.com>
2024-03-26 01:46:45 +0900

    update errorHandler logic

--- src/common/apis/axios.ts
+++ src/common/apis/axios.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import { HttpException } from '@nestjs/common';
+import axios, { AxiosError } from 'axios';
 import { config } from 'dotenv';
 
 config();
@@ -15,4 +16,24 @@ const axiosKakaoNav = axios.create({
   headers: { Authorization: process.env.KAKAO_API_KEY },
 });
 
-export { axiosKakaoMap, axiosKakaoNav };
+const errorHandler = (error: AxiosError) => {
+  //TODO: add & improve logging details
+  //TODO: move error handling functions to exception filters
+  if (error.response) {
+    // Server response status code was out of 2xx range
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+    throw new HttpException(error.response.data, error.response.status);
+  } else if (error.request) {
+    // Request successfully made but no response received
+    console.log(error.request);
+    throw new HttpException(error.request, 500);
+  } else {
+    // Request was not successfully made
+    console.log('Error', error.message);
+    throw new HttpException(error.message, 500);
+  }
+};
+
+export { axiosKakaoMap, axiosKakaoNav, errorHandler };
