bd23bff9fa02c90f3e5e492fa263b70b33dc6d3a
SeyunJang <ts-seyun.jang@rakuten.com>
2024-03-25 23:30:21 +0900

    add logger middleware

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    //temporary
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KakaoModule } from './kakao/kakao.module';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [KakaoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
