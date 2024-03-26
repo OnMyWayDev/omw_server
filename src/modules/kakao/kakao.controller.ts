9a263609bd145156fcadb45ae9fee676e480851f
SeyunJang <ts-seyun.jang@rakuten.com>
2024-03-26 21:40:35 +0900

    add sample driving-route api

--- src/modules/kakao/kakao.controller.ts
+++ src/modules/kakao/kakao.controller.ts
@@ -15,11 +15,35 @@ export class KakaoController {
 
   private readonly logger = new Logger(KakaoController.name);
 
+  @Get('cd-to-addr')
+  //returns list of candidate addresss (string value) with input coordinates (make sure coord system has to be converted)
+  //TODO: pipes can be added here, for validation
+  async getAddress(@Query() params: { x: string; y: string }) {
+    this.logger.log(`getAddress: ${params}`);
+    return await this.kakaoService.getAddress(params);
+  }
+
   @Get('keyword-search')
+  //returns list of place informations with input query, coordinates?, and raadius?
   //TODO: pipes can be added here, for validation
   async getKeywordSearch(@Query() params: KakaoKeywordSearchQuery) {
     this.logger.log(`getKeywordSearch: ${params}`);
     console.log(params, typeof params);
     return await this.kakaoService.getKeywordSearch(params);
+  }
+
+  @Get('driving-route')
+  //returns driving route information with input start and end coordinates (temporarily, no stopover)
+  async getDrivingRoute(
+    @Query()
+    params: {
+      originX: string;
+      originY: string;
+      goalX: string;
+      goalY: string;
+    },
+  ) {
+    this.logger.log(`getDrivingRoute: ${params}`);
+    return await this.kakaoService.getDrivingRoute(params);
   }
 }
--- src/modules/kakao/kakao.service.ts
+++ src/modules/kakao/kakao.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, HttpException } from '@nestjs/common';
-import { axiosKakaoMap, errorHandler } from 'src/common/apis/axios';
+import { axiosKakaoMap, axiosKakaoNav, errorHandler } from 'src/apis/axios';
 
 //TODO: remove duplicate in controller.ts
+//FIXME: move addresses to consts.ts
 interface KakaoKeywordSearchQuery {
   query: string;
   x?: string;
@@ -11,14 +12,56 @@ interface KakaoKeywordSearchQuery {
 
 @Injectable()
 export class KakaoService {
+  async getAddress(params: { x: string; y: string }) {
+    const { x, y } = params;
+    try {
+      const res = await axiosKakaoMap.get(
+        `geo/coord2address.json?x=${x}&y=${y}`,
+      );
+      return res.data;
+    } catch (err) {
+      errorHandler(err);
+    }
+  }
+
   async getKeywordSearch(params: KakaoKeywordSearchQuery) {
     const { query, x, y, radius } = params;
+    //FIXME: validate query, x, y, radius (must exist, string)
     //TODO: to update into try...catch block
     //TODO: move api functions to api directory
     try {
-      const res = await axiosKakaoMap.get(
-        `search/keyword.JSON?query=${query}${x ? `&x=${x}` : ''}${y ? `&y=${y}` : ''}${radius ? `&radius=${radius}` : ''}`,
-      );
+      const res = await axiosKakaoMap.get('search/keyword.JSON', {
+        // NOTE: params that are null or undefined are not rendered in the URL.
+        params: {
+          query: query,
+          x: x,
+          y: y,
+          radius: radius,
+        },
+      });
+      return res.data;
+    } catch (err) {
+      errorHandler(err);
+    }
+  }
+
+  async getDrivingRoute(params: {
+    originX: string;
+    originY: string;
+    goalX: string;
+    goalY: string;
+  }) {
+    //Suppose no stopover for now, temporarily
+    const { originX, originY, goalX, goalY } = params;
+    try {
+      //FIXME: update axios code to use params option instead of single string
+      const res = await axiosKakaoNav.get('directions', {
+        params: {
+          origin: `${originX},${originY}`,
+          destination: `${goalX},${goalY}`,
+        },
+      });
+      // this.logger.log(res.data.) -> 총 vertex수 세기
       return res.data;
     } catch (err) {
       errorHandler(err);
