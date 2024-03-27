01b3044195df789d44c53e5957a1e79f644fb585
Seyun Jang <kj10522002@gmail.com>
2024-03-27 23:32:33 +0900

    extend request body size limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ limit: '5mb', extended: true }));
   await app.listen(3000);
 }
 bootstrap();
