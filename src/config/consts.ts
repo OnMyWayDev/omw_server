0a8e9e804d6d433cd3029962beeeb1446894a204
Seyun Jang <kj10522002@gmail.com>
2024-03-27 22:38:10 +0900

    add kakaoAPIURL consts

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -2,7 +2,11 @@ import { config } from 'dotenv';
 
 config();
 
-//constant values here
+export const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
+
 export const KAKAO_MAP_BASE_URL = 'https://dapi.kakao.com/v2/local/';
 export const KAKAO_NAV_BASE_URL = 'https://apis-navi.kakaomobility.com/v1/';
-export const KAKAO_API_KEY = process.env.KAKAO_API_KEY;
+
+export const KAKAO_GET_ADDRESS_URL = 'geo/coord2address.JSON';
+export const KAKAO_GET_DRIVING_ROUTE_URL = 'directions';
+export const KAKAO_KEYWORD_SEARCH_URL = 'search/keyword.JSON';
