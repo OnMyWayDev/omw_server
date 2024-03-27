595590a1e0d4d1ab34a754e87510688ac10cb18d
Seyun Jang <kj10522002@gmail.com>
2024-03-27 23:06:31 +0900

    update searchOnPath API, move comments to NOTE below code

--- src/modules/kakao/kakao.service.ts
+++ src/modules/kakao/kakao.service.ts
@@ -1,6 +1,4 @@
 import { Injectable, HttpException } from '@nestjs/common';
-import { AxiosError } from 'axios';
-import { axiosKakaoMap, axiosKakaoNav, errorHandler } from 'src/apis/axios';
 import kakaoGetAddress from 'src/apis/kakaoGetAddress';
 import kakaoGetDrivingRoute from 'src/apis/kakaoGetDrivingRoute';
 import kakaoKeywordSearch from 'src/apis/kakaoKeywordSearch';
@@ -17,21 +15,14 @@ import {
 import removeDuplicate from 'src/helpers/removeDuplicate';
 import selectVertices from 'src/helpers/selectVertices';
 
-//TODO: remove duplicate in controller.ts
-//FIXME: move addresses to consts.ts
-
 @Injectable()
 export class KakaoService {
   async getAddress(params: KakaoGetAddressQuery) {
-    //convert coordinates to address strings
     const data = await kakaoGetAddress(params);
     return data;
   }
 
   async getKeywordSearch(params: KakaoKeywordSearchQuery) {
-    //TODO: Utilize 'category' parameter, according to confirmed UX flows.
-    //FIXME: validate query, category, x, y, radius (must exist, string)
-    //FIXME: result data has to be differently handled according to purpose.
     const data = await kakaoKeywordSearch(params);
     //TODO: add more logics here
     return data;
@@ -40,19 +31,11 @@ export class KakaoService {
   async getDrivingRoute(
     params: GetDrivingRouteQuery,
   ): Promise<GetDrivingRouteResult> {
-    //as of now, Suppose Alternatives = false
-    //FIXME: UX -> why don't we show multiple possible routes
-    //input: start, end, waypoints, alternatives, avoid
-    //output: vertexList = [ [x1, y1], [x2, y2], ... ], totalDistance, totalDuration
-    //FIXME: utilize 'alternative' -> in case, result data has to be different.
-    //FIXME: params validation
-    //TODO: list up exceptions (for client), add exception handling logic
     const data = await kakaoGetDrivingRoute(params);
     if (data.routes[0].result_code !== 0) {
-      //If Route Search Failed, FIXME: add exception handling logic
       throw new HttpException(data.routes[0].result_msg, 400);
     }
-    //Route Search Succeeded
+
     const res = { result: { duration: -1, distance: -1, path: [] } };
     const route = data.routes[0];
     const duration = route.summary.duration;
@@ -78,100 +61,98 @@ export class KakaoService {
   }
 
   async getStopByDuration(params: KakaoDrivingPathQuery) {
-    //sets summary = true, alternative = false, returns duration only
     const data = await kakaoGetDrivingRoute({
       ...params,
       summary: true,
       alternatives: false,
     });
     if (data.routes[0].result_code !== 0) {
-      //Route Search Failed
-      //TODO: add exception handling logic
       throw new HttpException(data.routes[0].result_msg, 400);
     }
     return data.routes[0].summary.duration;
   }
 
-  //FIXME: Actual Logic has to be differently implemented.
-  //1. Server: getDrivingRoute() => returns vertex list, totalDistance and other informations to client
-  //2. Client: get input KeywordQuery, Desired Category, Radius from user.
-  //Minimum/Maximum Radius has to be calculated based on totalDistance
-  //e.g) ( totalDistance / 20 ) <= Radius <= min(200000, max(1000, totalDistance))
-  //3. Server: searchOnPath(vertexList, totalDistance) -> repeat keywordSearch() for each vertex -> return all results
-  // async searchOnPath(params: SearchOnPathQuery) {
-  // let searchResults = []; // list to return
-  //default Radius값 필요
-  //   const { query, category, startX, startY, endX, endY } = params;
-  //   const radius = parseInt(params.radius); //TODO: add pipe to validate, convert to number (not to use parseInt)
-  //   //radius has to be given as int, in 'meter' unit
-  //   const data = await this.getDrivingRoute({ startX, startY, endX, endY });
-  //   //routes[0].result_code로 분기 가능
-  //   if (data.routes[0].result_code !== 0) {
-  //     //분기 로직 실행
-  //     // throw new HttpException(data.routes[0].result_message, 400);
-  //   } else {
-  //     //길찾기 성공시
-  //     const roads = data.routes[0].sections[0].roads;
-  //     const tmpVertexList = [];
-  //     roads.map((road) => {
-  //       tmpVertexList.push(...road.vertexes);
-  //     });
-  //     const vertexList = tmpVertexList.reduce((acc, cur, idx) => {
-  //       if (idx % 2 === 0) acc.push([cur]);
-  //       else acc[acc.length - 1].push(cur);
-  //       return acc;
-  //     }, []);
-  //     const totalDistance = parseInt(data.routes[0].summary.distance);
+  async searchOnPath(params: SearchOnPathQuery) {
+    const { query, category, radius, path, totalDistance } = params;
+    const selectedVertices: string[][] = selectVertices({
+      path,
+      totalDistance,
+      radius,
+    });
 
-  //     //decide which vertices to use for keyword search
-  //     const selectedVertices: string[][] = selectVertices({
-  //       vertexList,
-  //       totalDistance,
-  //       radius,
-  //     });
+    const promises = selectedVertices.map((vertex) =>
+      this.getKeywordSearch({
+        query: query,
+        x: vertex[0],
+        y: vertex[1],
+        radius: radius.toString(),
+      }),
+    );
 
-  //     //getKeywordSearch() for each selectedVertex
-  //     //res.data.documents 싹다 모으기 (res.data.documents => place_name, place_url, x, y 값만)
-  //     //search keyword on selected vertices
-  //     //실패하더라도 다른 결과값은 반환해야 함 -> ./api폴더로 api들 옮기고, 서비스 로직이랑은 분리해서 사용하기. HttpRequestError반환 x
-  //     //단 여러번의 검색 수행 중 에러난게 있었다면, 일부 에러가 있었다는 정보를 반환해야 함
+    const results = await Promise.allSettled(promises);
+    const successfulResults = results.filter(
+      (result) => result.status === 'fulfilled',
+    );
 
-  //     // Below code takes 2.31 seconds (Seoul-Gangneung, 꽃), 3.5 seconds (Seoul-Busan, 꽃)
-  //     const promises = selectedVertices.map((vertex) =>
-  //       this.getKeywordSearch({
-  //         query: query,
-  //         x: vertex[0],
-  //         y: vertex[1],
-  //         radius: radius.toString(),
-  //       }),
-  //     );
+    if (successfulResults.length < promises.length - 2) {
+      throw new Error('More than 2 requests failed');
+    }
 
-  //     const results = await Promise.allSettled(promises);
+    const searchResults = [];
+    successfulResults.forEach((result: PromiseFulfilledResult<any>) => {
+      result.value.documents.map((document) => {
+        searchResults.push({
+          place_name: document.place_name,
+          place_url: document.place_url,
+          x: document.x,
+          y: document.y,
+        });
+      });
+    });
 
-  //     const successfulResults = results.filter(
-  //       (result) => result.status === 'fulfilled',
-  //     );
+    const res = removeDuplicate(searchResults);
 
-  //     if (successfulResults.length < promises.length - 2) {
-  //       //TODO: add error handling Logic
-  //       throw new Error('More than 2 requests failed');
-  //     }
-
-  //     successfulResults.forEach((result: PromiseFulfilledResult<any>) => {
-  //       result.value.documents.map((document) => {
-  //         searchResults.push({
-  //           place_name: document.place_name,
-  //           place_url: document.place_url,
-  //           x: document.x,
-  //           y: document.y,
-  //         });
-  //       });
-  //     });
-
-  //     //remove duplicates
-  //     searchResults = removeDuplicate([...searchResults]);
-  //   }
-
-  //   return searchResults;
-  // }
+    return res;
+  }
 }
+
+// -------- NOTES ---------
+
+// --- GETADDRESS ---
+//convert coordinates to address strings
+
+// --- GETKEYWORDSEARCH ---
+//TODO: Utilize 'category' parameter, according to confirmed UX flows.
+//FIXME: validate query, category, x, y, radius (must exist, string)
+//FIXME: result data has to be differently handled according to purpose.
+//FIXME: seperate logic for purposes
+//1. get coordiates from address
+//2. search for keywords => more optional parameters has to be considered!! (페이지량, 정보량 등...)
+//3. ETC...
+
+// --- GETDRIVINGROUTES ---
+//as of now, Suppose Alternatives = false
+//FIXME: UX -> why don't we show multiple possible routes
+//input: start, end, waypoints, alternatives, avoid
+//output: vertexList = [ [x1, y1], [x2, y2], ... ], totalDistance, totalDuration
+//FIXME: utilize 'alternative' -> in case, result data has to be different.
+//FIXME: params validation
+//TODO: list up exceptions (for client), add exception handling logic
+
+// --- SEARCHONPATH ----
+//TODO: add pipe to validate radius and totalDistance, convert to number (not to use parseInt)
+//FIXME: default Radius값 필요
+//TODO: add error handling Logic
+
+// --- GETSTOPBYDURATION ---
+//sets summary = true, alternative = false, returns duration only
+//Route Search Failed
+//TODO: add exception handling logic
+
+// ---- searchOnPath Logic -----
+//1. Server: getDrivingRoute() => returns vertex list, totalDistance and other informations to client
+//2. Client: get input KeywordQuery, Desired Category, Radius from user.
+//Minimum/Maximum Radius has to be calculated based on totalDistance
+//e.g) ( totalDistance / 20 ) <= Radius <= min(200000, max(1000, totalDistance))
+//3. Server: searchOnPath(vertexList, totalDistance) -> repeat keywordSearch() for each vertex -> return all results
+//If any error occurs during multiple keywordsearchRequests, Flag should be set to show there was ERROR tho.
