a4e2c86bf6795f3ff7010f47c2686c025e449363
SeyunJang <ts-seyun.jang@rakuten.com>
2024-04-07 17:53:12 +0900

    temporarily disable mongoose (network issue)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { MapModule } from './modules/map/map.module';
 
 //TODO: add malicious attack protection with guard?helmet? with middlewares or interceptors
 //TODO: CORS, CSRF, and other security measures
@@ -15,10 +16,10 @@ import { UserModule } from './modules/user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_DB_URI), //connect to MongoDB, TODO: utilize DB for logging and user auth, current db name : ~/omw
-    //useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
+    // MongooseModule.forRoot(process.env.MONGO_DB_URI), //connect to MongoDB, TODO: utilize DB for logging and user auth, current db name : ~/omw
     KakaoModule,
-    UserModule,
+    // UserModule,
+    MapModule,
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -27,6 +28,6 @@ export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
-    mongoose.set('debug', this.isDev); //debugging mongoose in development
+    // mongoose.set('debug', this.isDev); //debugging mongoose in development
   }
 }
