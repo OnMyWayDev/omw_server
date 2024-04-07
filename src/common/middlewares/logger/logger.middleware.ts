2cbd99ddc430be35d67a5f45c927cec6191e3f32
SeyunJang <ts-seyun.jang@rakuten.com>
2024-04-07 17:53:19 +0900

    improve logger middleware

--- src/common/middlewares/logger/logger.middleware.ts
+++ src/common/middlewares/logger/logger.middleware.ts
@@ -4,15 +4,18 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
-  //middleware logic
+
   use(req: Request, res: Response, next: NextFunction) {
-    // this.logger.log(req.ip, req.originalUrl, req.method, req.body);
-    // console.log(req.ip, req.originalUrl, req.method, req.body);
-    //TODO: improve overall quality, log details
-    //TODO: write logs to DB
-    //TODO: write log when HTTPError occurs
+    const { ip, method, originalUrl, params, body } = req;
+    this.logger.log(
+      `Request Info - IP: ${ip}, Method: ${method}, URL: ${originalUrl}, Params: ${JSON.stringify(params)}, ${body && 'Body: ' + JSON.stringify(body)}`,
+    );
+
     res.on('finish', () => {
-      this.logger.log(res.statusCode, res.statusMessage);
+      const { statusCode, statusMessage } = res;
+      this.logger.log(
+        `Response Info - StatusCode: ${statusCode}, StatusMessage: ${statusMessage}`,
+      );
     });
 
     next();
