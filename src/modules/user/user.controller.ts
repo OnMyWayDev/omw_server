e8ab008ccc53c6db22ae27cc4fc216fae6ab6aa9
SeyunJang <ts-seyun.jang@rakuten.com>
2024-04-11 10:21:00 +0900

    add summary to user.signup

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserSignUpDto } from './dto/user.request.dto';
 import { UserService } from './user.service';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserReadOnlyDto } from './dto/user.dto';
 
 @Controller('user')
@@ -12,6 +12,7 @@ export class UserController {
   @Post('/sign-up')
   @ApiResponse({ status: 201, description: 'Created', type: UserReadOnlyDto })
   @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiOperation({ summary: 'Draft (not used yet)' })
   async signUp(@Body() body: UserSignUpDto) {
     return await this.userService.signUp(body);
   }
