02d97d7e18391a6218e1eac4e6cf35be6c59fa07
SeyunJang <ts-seyun.jang@rakuten.com>
2024-04-16 17:11:04 +0900

    add category list to type (for future usage)

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -12,3 +12,26 @@ export const KAKAO_GET_DRIVING_ROUTE_URL = 'directions';
 export const KAKAO_KEYWORD_SEARCH_URL = 'search/keyword.JSON';
 
 export const ROUTE_PRIORITY_LIST = ['RECOMMEND', 'TIME', 'DISTANCE'];
+
+//MT1=대형마트 / CS2=편의점 / PS3=어린이집, 유치원 / SC4=학교 / AC5=학원 / PK6=주차장 / OL7=주유소, 충전소 / SW8=지하철역 / BK9=은행 / CT1=문화시설 / AG2=중개업소 / PO3=공공기관 / AT4=관광명소 / AD5=숙박 / FD6=음식점 / CE7=카페 / HP8=병원 / PM9=약국
+export const CATEGORY_LIST = [
+  'MT1',
+  'CS2',
+  'PS3',
+  'SC4',
+  'AC5',
+  'PK6',
+  'OL7',
+  'SW8',
+  'BK9',
+  'CT1',
+  'AG2',
+  'PO3',
+  'AT4',
+  'AD5',
+  'FD6',
+  'CE7',
+  'HP8',
+  'PM9',
+];
+//FIXME: fix it to be enum or literals
