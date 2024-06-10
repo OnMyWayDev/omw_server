191406f3f3e2e5c82257ec6d4332dba5b3066a0f
SeyunJang <ts-seyun.jang@rakuten.com>
2024-06-11 03:34:44 +0900

    update removeDpulicate.ts

--- src/helpers/removeDuplicate.ts
+++ src/helpers/removeDuplicate.ts
@@ -1,7 +1,11 @@
 const removeDuplicate = (arr: any[]) => {
+  //address_name과 place_name이 같은 경우 중복으로 처리
   return arr.filter(
     (v, i, a) =>
-      a.findIndex((t) => JSON.stringify(t) === JSON.stringify(v)) === i,
+      a.findIndex(
+        (t) =>
+          t.address_name === v.address_name && t.place_name === v.place_name,
+      ) === i,
   );
 };
 
