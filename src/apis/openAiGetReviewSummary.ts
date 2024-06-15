0bfcfc33a0f57165bfd9269c7954b43f165c230c
SeyunJang <ts-seyun.jang@rakuten.com>
2024-06-16 07:07:56 +0900

    update reviewsummarybot

--- src/apis/openAiGetReviewSummary.ts
+++ src/apis/openAiGetReviewSummary.ts
@@ -1,11 +1,23 @@
 import { OPENAI_API_URL } from 'src/config/consts';
 import { axiosOpenAi, errorHandler } from './axios';
 
-const openAiGetReviewSummary = async (params: any) => {
+const openAiGetReviewSummary = async (params: { corpus: string }) => {
   try {
-    const res = await axiosOpenAi.post(OPENAI_API_URL, {
-      ...params, //FIXME:fixme
-    });
+    const body = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content:
+            '너는 리뷰들을 요약해주는 요약 봇이야. 보내주는 리뷰들을 300~400자 사이로 요약하되 존댓말로 생성해.',
+        },
+        {
+          role: 'user',
+          content: params.corpus, //FIXME: add enhanced preprocessing logic here
+        },
+      ],
+    };
+    const res = await axiosOpenAi.post(OPENAI_API_URL, body);
     return res.data;
   } catch (err) {
     console.log('Error occured in openAiGetReviewSummary :', err);
