7458662e9a2aa233108284a7912f198a005c81d3
SeyunJang <ts-seyun.jang@rakuten.com>
2024-06-16 07:14:57 +0900

    update maximum length of review summary

--- src/apis/openAiGetReviewSummary.ts
+++ src/apis/openAiGetReviewSummary.ts
@@ -9,7 +9,7 @@ const openAiGetReviewSummary = async (params: { corpus: string }) => {
         {
           role: 'system',
           content:
-            '너는 리뷰들을 요약해주는 요약 봇이야. 보내주는 리뷰들을 300~400자 사이로 요약하되 존댓말로 생성해.',
+            '너는 리뷰들을 요약해주는 요약 봇이야. 보내주는 리뷰들을 최대 300자 이하로 요약하되 존댓말로 생성해.',
         },
         {
           role: 'user',
