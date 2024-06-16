4333f19ae7f90001b98c32840b5a6f16e9128a63
SeyunJang <ts-seyun.jang@rakuten.com>
2024-06-17 06:48:58 +0900

    adjust maximum value

--- src/modules/map/map.service.ts
+++ src/modules/map/map.service.ts
@@ -227,7 +227,10 @@ export class MapService {
         x: vertex[0],
         y: vertex[1],
         radius: radius.toString(),
-        size: Math.min(Math.ceil(80 / selectedVertices.length), 15).toString(), //지점당 검색 결과 개수,, temporary
+        size: Math.min(
+          Math.ceil(maximum + 10 / selectedVertices.length),
+          15,
+        ).toString(), //지점당 검색 결과 개수,, temporary
         category_group_code,
       }),
     );
